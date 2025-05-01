59107ce32b1195e86875c62b2fc7f27dcbf93399
Reshetov <vlareshka@mail.ru>
2025-05-01 18:49:51 +0400

    adasaada

--- scenes/level1/scripts/death_zone.gd
+++ scenes/level1/scripts/death_zone.gd
@@ -3,7 +3,8 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _on_body_entered(body):
 	if body.name == "Cat":
-		await get_tree().create_timer(1).timeout
-		Global.lose_life()
+		$"../Sounds/Music".stop()
+		body.die()
+		
 		
 		
