948c011b026602d6777b3c3da472d9ed4996a3ad
Gridyaev <gr.oleg.721@gmail.com>
2025-05-11 17:08:19 +0400

    фывфвыhyфывфывnyapizdawмыаer

--- scenes/level1/scripts/level_1.gd
+++ scenes/level1/scripts/level_1.gd
@@ -5,7 +5,7 @@ var save_path = "res://savegame.save"
 @onready var player = $Cat
 
 func _ready():
-	anim_player.play("fade_in")
+	#anim_player.play("fade_in")
 	Global.game_over.connect(show_death_screen)
 	load_game()
 	
@@ -16,6 +16,7 @@ func show_death_screen():
 	add_child(death_screen)
 	
 func load_game():
+	pass
 	var file = FileAccess.open(save_path, FileAccess.READ)
 	player.position.x = file.get_var(player.position.x)
 	player.position.y = file.get_var(player.position.y)
