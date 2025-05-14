c63578a8a4e184e5b19cc6ec8e3f097a63c48f34
Skuyno <143557346+Skuyno@users.noreply.github.com>
2025-05-15 02:02:07 +0400

    Machinki final

--- scenes/level2/scripts/level_2.gd
+++ scenes/level2/scripts/level_2.gd
@@ -1,6 +1,21 @@
 extends Node2D
+@onready var anim_player: AnimationPlayer = $Launching/AnimationPlayer
+var save_path = "res://savegame.save"
+@onready var player = $Cat
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	$Sounds/Music.play()
+	Global.game_over.connect(show_death_screen)
+	
+
+func show_death_screen():
+	var death_screen = preload("res://scenes/death_screen_menu/death_screen.tscn").instantiate()
+	
+	add_child(death_screen)
+	
+func load_game():
+
+	var file = FileAccess.open(save_path, FileAccess.READ)
+	player.position.x = file.get_var(player.position.x)
+	player.position.y = file.get_var(player.position.y)
+	
