24258bdb01043af0ce8f6ef3718ecc7cf60a9fab
Gridyaev <gr.oleg.721@gmail.com>
2025-05-16 18:49:01 +0400

    sdfsdтттf

--- scenes/death_screen_menu/scripts/death_screen.gd
+++ scenes/death_screen_menu/scripts/death_screen.gd
@@ -2,8 +2,10 @@ extends CanvasLayer
 
 @onready var anim_player = $AnimationPlayer
 @onready var dead_cat_sprite = $DeadCatSprite
-@onready var buttons_container = $ButtonsContainer
+@onready var buttons_container = $ButtonsContainer2
 @onready var delay_timer = $DelayTimer
+var save_path = "res://savegame.save"
+@onready var player = $Cat
 
 func _ready():
 	$AudioStreamPlayer.play()
@@ -28,3 +30,12 @@ func _on_restart_button_pressed():
 func _on_menu_button_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/menu/menu.tscn")
+
+
+func _on_load_pressed() -> void:
+	get_tree().paused = false
+	Global.reset()
+	# Эмитируем сигнал или вызываем метод загрузки в сцене уровня
+	get_tree().current_scene.load_game()
+	queue_free() # Удаляем экран смерти
+	
